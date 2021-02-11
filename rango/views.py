94cf7b0761e70f0c2b4242cbd5de351238a8070e
Richard Bozko <2472271b@student.gla.ac.uk>
2021-02-11 19:31:58 +0000

    chapter 8

--- rango/views.py
+++ rango/views.py
@@ -58,7 +58,7 @@ def show_category(request, category_name_slug):
 
 	return render(request, 'rango/category.html', context = context_dict)
 	
-@login_required
+#@login_required
 def add_category(request):
 	form = CategoryForm()
 
@@ -75,7 +75,7 @@ def add_category(request):
 	return render(request,'rango/add_category.html', {'form': form}) 
 
 
-@login_required
+#@login_required
 def add_page(request, category_name_slug):
 	try:
 		category = Category.objects.get(slug = category_name_slug)
@@ -163,12 +163,12 @@ def some_view(request):
 	else:
 		return HttpResponse("You are not logged in.")
 
-@login_required
+#@login_required
 def restricted(request):
 	return render(request, 'rango/restricted.html', {})
 
 
-@login_required
+#@login_required
 def user_logout(request):
 	logout(request)
 	return redirect(reverse('rango:index'))
